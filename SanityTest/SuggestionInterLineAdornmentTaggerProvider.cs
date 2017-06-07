4c6a361a9611f81b20892cbf759e1e8052c9f9cf
Betai Koffi <betai@xamarin.com>
2017-06-07 11:45:53 -0700

    after

--- SanityTest/SuggestionInterLineAdornmentTaggerProvider.cs
+++ SanityTest/SuggestionInterLineAdornmentTaggerProvider.cs
@@ -1,8 +1,22 @@
-﻿namespace SanityTest
+﻿using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Tagging;
+
+namespace SanityTest
 {
 
-    internal class SuggestionInterLineAdornmentTaggerProvider
+    internal class SuggestionInterLineAdornmentTaggerProvider : IViewTaggerProvider
     {
+        public ITagger<T> CreateTagger<T>(ITextView textView, ITextBuffer buffer) where T : ITag
+        {
+            if (textView.TextBuffer == buffer)
+            {
+                //return SuggestionInterLineAdornmentTagger.GetorCreate(this, textView) as ITagger<T>;
+                return null; // TODO: eventually replace with this ^
+            }
+
+            return null;
+        }
     }
 }
 
